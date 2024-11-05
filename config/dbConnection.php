7c2cf2e20022ba0759d24d6287f370be4a808e1e
Arkaitz <abads651@gmail.com>
2024-11-05 10:16:27 +0100

    Añadido el archivo de prueba de conexion a la base de datos

---
+++ config/dbConnection.php
@@ -0,0 +1,23 @@
+<?php
+
+function getDBConnection(){
+$host = "localhost";
+$db_name = "Proyecto";
+$username = "samuel";
+$password = "root";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexion exitosa";
+    return $conn;
+} catch (PDOException $e) {
+    echo 'Error de conexión: ' . $e->getMessage();
+    return null;
+}
+
+}
+
+
+
+?>
