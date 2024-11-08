253cc5b75ff41c10701cbc088cebf89e0b25f0d1
sabad80 <abads651@gmail.com>
2024-11-08 17:47:39 +0100

    Añadi lo de mostrar el nombre de usuario (en el home)

--- app/view/home.php
+++ app/view/home.php
@@ -1,3 +1,6 @@
+<?php
+    session_start(); // Iniciamos la sesión para mostrar el nombre del usuario
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +9,13 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Home page</h1>
+    <?php
+    if (isset($_SESSION['nombreUsuario'])) {
+        $username = $_SESSION['nombreUsuario'];
+        echo " <h3>Bienvenido, " . htmlspecialchars($username) . "!<h3>";
+    } else {
+        echo "No has iniciado sesión.";
+    }
+    ?>
 </body>
 </html>
