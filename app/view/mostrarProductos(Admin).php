892589bcea8dc4350a309854d595a92de8bd9f06
sabad80 <abads651@gmail.com>
2024-12-02 21:34:34 +0100

    Segundo intento css mostrarProductos(admin)

--- app/view/mostrarProductos(Admin).php
+++ app/view/mostrarProductos(Admin).php
@@ -9,7 +9,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/motrarProductos.css">
+    <link rel="stylesheet" href="css/mostrarProductos.css">
 </head>
 
 <body>
